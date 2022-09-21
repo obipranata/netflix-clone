1408ea0575753b1f45775f154462b41f54a3e0fc
Obi Pranata <obipranata@192.168.1.7>
2022-09-22 01:12:45 +0900

    first

---
+++ src/Banner.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import requests from './request';
+import "./Banner.css";
+import movieTrailer from 'movie-trailer';
+import YouTube from 'react-youtube';
+
+function Banner() {
+	const [movie, setMovie] = useState([]);
+	const [trailerUrl, setTrailerUrl] = useState("");
+
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(requests.fetchTrending);
+
+			setMovie(
+				request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]
+			);
+		}
+		fetchData();
+	}, []);
+
+	const opts = {
+		height: "450",
+		width: "100%",
+		playerVars: {
+			autoplay: 1
+		}
+	}
+
+	const handleClick = (movie) => {
+		if (trailerUrl) {
+			setTrailerUrl('');
+		} else {
+			movieTrailer(movie?.title || movie?.name || movie?.original_title || "")
+				.then(url => {
+					const urlParams = new URLSearchParams(new URL(url).search);
+					setTrailerUrl(urlParams.get('v'));
+				}).catch(error => console.log(error));
+		}
+	}
+
+	function truncate(str, n) {
+		return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+	}
+
+	return (
+		<header
+			className="banner"
+			style={{
+				backgroundSize: "cover",
+				backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+				backgroundPosition: "center center"
+			}}
+		>
+			{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+			<div className="banner__contents">
+				<h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+				<div className="banner__buttons">
+					<button className="banner__button" onClick={() => handleClick(movie)}>Play</button>
+					<button className="banner__button">My List</button>
+				</div>
+
+				<h1 className="banner__description">
+					{truncate(movie?.overview, 150)}
+				</h1>
+			</div>
+
+			<div className="banner--fadeBottom" />
+		</header>
+	)
+}
+
+export default Banner
---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+	const [show, handleShow] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener("scroll", () => {
+			if (window.scrollY > 100) {
+				handleShow(true);
+			} else handleShow(false);
+		});
+		// return () => {
+		// 	window.removeEventListener("scroll");
+		// }
+	}, []);
+
+	return (
+		<div className={`nav ${show && "nav__black"}`}>
+			<img
+				className="nav__logo"
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
+				alt="Netflix Logo"
+			/>
+			<img
+				className="nav__avatar"
+				src="https://i.pinimg.com/564x/bd/ee/4c/bdee4c328550aaf21aa9f43fd19e2136.jpg"
+				alt="Netflix Logo"
+			/>
+		</div>
+	)
+}
+
+export default Nav
